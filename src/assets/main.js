284ce9414bfd0980f08a46f2b5dba8c9eee33f92
Surabhi Kumari <M1039307@mindtree.com>
2017-02-18 15:27:49 +0530

    till step 11 i have performed

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,4 +6,34 @@ function guess() {
     //add functionality to guess function here
 }
 
-//implement new functions here
+function setHiddenFields() {
+    answer = Math.floor((Math.random() * 100000) + 1);
+    var str = answer.toString();
+    while (str.length() < 4) {
+        str = '0' + str;
+    }
+    attempt = 0;
+}
+
+function guess() {
+    if (answer == '' || attempt == ' ')
+        setHiddenFields();
+}
+
+function setMessage(answer) {
+    messsage = document.getElementByName(answer).innerHTML;
+}
+
+function validateInput(answer) {
+    if (answer.length == 4) {
+        return true;
+    } else {
+        setMessage("Guesses must be exactly 4 characters long.");
+        return false;
+    }
+}
+if (validateInput(input.value) == true) {
+    attempt = attempt + 1;
+} else {
+    return false;
+}
